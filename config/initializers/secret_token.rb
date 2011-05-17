02067f39d63c84b88f6382ce9ceaf6f3fd8a150e
Coder <weyewe@Willy-Yunnals-MacBook-Pro.local>
2011-05-17 07:04:21 +0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plant::Application.config.secret_token = '7944610c80954235d774622d2abe4e3d37b63098421e24c17ade52b465a1f28519c60491ba40227e882225434bc822df2f3aaa0a50776fa88bbfd91dd8d2ce05'
